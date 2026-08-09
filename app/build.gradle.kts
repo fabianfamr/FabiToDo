d2e24176bbf60a667a665cfa395fff2b9e9e935a
Fabian ツ <fabianajjdvd@gmail.com>
2026-08-09 13:28:07 -0400

    chore: bump version to 1.8.3 and cleanup CI scripts
    Update project version to 1.8.3 and minify dummy google-services.json creation in GitHub Actions workflows.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.fabian.todolist"
     minSdk = 24
     targetSdk = 36
-    versionCode = 20
-    versionName = "1.8.2"
+    versionCode = 21
+    versionName = "1.8.3"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     buildConfigField("String", "GEMINI_API_KEY", "\"\"")
   }
